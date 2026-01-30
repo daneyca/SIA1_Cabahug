02dac39158ec1b1aff221092ee22dcd9cbb4bce8
daneyca <danicamieee24@gmail.com>
2026-01-30 16:47:17 +0800

    Customize users table migration and model

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,11 +17,14 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+  protected $fillable = [
+    'name',
+    'email',
+    'password',
+    'role',
+    'is_active',
+];
+
 
     /**
      * The attributes that should be hidden for serialization.
@@ -39,7 +42,8 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+    'email_verified_at' => 'datetime',
+    'is_active' => 'boolean',
+];
+
 }
